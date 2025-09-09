b0943b5a739eb336eabe681f34d0bbd0d4d8b02b
Daniel Lai <laidaniel759@gmail.com>
2025-09-09 14:43:53 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ export default function App() {
   const [todos, setTodos] = useLocalStorage('todos', [])
   return (
     <TodoContext.Provider value={{ todos, setTodos }}>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
@@ -38,7 +38,7 @@ export default function App() {
           </Route>
 
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </TodoContext.Provider>
   )
 }
